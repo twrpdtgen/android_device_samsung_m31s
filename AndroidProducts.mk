b238cd5274eddb280c4a918508b4a01481bc8ace
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-30 13:23:49 +0200

    m31s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-M317F Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_m31s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_m31s-user \
+    omni_m31s-userdebug \
+    omni_m31s-eng
